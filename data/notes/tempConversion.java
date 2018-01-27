be96452c6bd81c21a8fa9e739e40ecd2a56ee6f0
Garrett Dancik <garrett.dancik@gmail.com>
2018-01-27 16:35:31 -0500

    Update

--- data/notes/tempConversion.java
+++ data/notes/tempConversion.java
@@ -10,13 +10,13 @@ public class tempConversion {
 		// Setup scanner to get input from keyboard	
 		Scanner scnr = new Scanner(System.in); 
 
-		// prompt the user to enter a temperature in degrees Celsius
+		// prompt the user to enter an integer temperature in degrees Celsius,
+        // and read in the temperature from the user
 		
 		// convert from Celsius to Fahrenheit, using the formula F = 9/5C + 32
 		
 		// output the temperature in degrees Fahrenheit, using the format
-		// 0 degrees Celsius is 32 degrees Fahrenheit
-		
+		// "0 degrees Celsius is 32 degrees Fahrenheit"
 		
 		scnr.close();
 	}
